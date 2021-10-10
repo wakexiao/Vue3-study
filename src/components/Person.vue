c7565383f7f049ada7f1ab2cfe87ff7adcc78e54
xunliu <1970801468@qq.com>
2021-10-10 14:03:57 +0800

    feat: 实现数据双向绑定demo

---
+++ src/components/Person.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>姓名：{{name}}</div>
+  <div>年龄：{{age}}</div>
+  <button @click="addAge">年龄加一</button>
+</template>
+
+<script>
+import {reactive, toRef, toRefs} from 'vue'
+export default {
+    setup(){
+        const person = reactive({
+            name: 'jack',
+            age: 18
+        })
+        function addAge(){
+            // console.log(person)
+            person.age ++
+        }
+        return {
+            // person,
+            // name: toRef(person, 'name'),
+            // age: toRef(person, 'age'),
+            ...toRefs(person),
+            // ...person,
+            addAge
+        }
+    }
+}
+</script>
